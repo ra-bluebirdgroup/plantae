1b8043cd671818c65c65b2d1308ccf41f2a690b7
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-11-11 18:14:55 -0500

    ios camera test 2

--- src/components/identifier.jsx
+++ src/components/identifier.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Axios from 'axios';
 import Camera from 'react-html5-camera-photo';
 import CameraIos, { DEVICE, FACING_MODE, PLACEMENT } from 'react-camera-ios';
+import {isMobile} from 'react-device-detect';
 import 'react-html5-camera-photo/build/css/index.css';
 import PlantsContainer from "./plants_container";
 import TextLoop from "react-text-loop";
@@ -33,24 +34,42 @@ class Identifier extends React.Component {
  }
 
  takePhoto = () => {
-   const containerStyle = { display: 'flex', height: '300px', width: '300px' };
-
-  this.setState({
-     camera:
-      <div style={containerStyle}>
-           <Camera
-        device={DEVICE.MOBILE}
-        facingMode={FACING_MODE.ENVIRONMENT}
-        placement={PLACEMENT.COVER}
-        quality="1"
-        onError={error => console.log(error)}
-        onTakePhoto = { (dataUri) => {
-          this.setState({ queryImage: dataUri},()=> this.postIdentification())
-        } }
-         />
-       </div>,
-        wol: false
+   console.log(isMobile)
+  if (isMobile) {
+  const containerStyle = { display: 'flex', height: '300px', width: '300px' };
+   this.setState({
+     camera: <div style={containerStyle}>
+    <CameraIos
+      device={DEVICE.MOBILE}
+      facingMode={FACING_MODE.ENVIRONMENT}
+      placement={PLACEMENT.COVER}
+      quality="1"
+      onError={error => console.log(error)}
+      onTakePhoto = { (dataUri) => {
+        this.setState({
+          queryImage: dataUri,
+        },()=> this.postIdentification())
+      } }
+    />
+  </div>,
+   wol: false
    })
+
+  } else {
+     this.setState({
+     wol: false,
+     camera:
+     <Camera
+     imageType = {"jpg"}
+     onTakePhoto = { (dataUri) => {
+       this.setState({
+         queryImage: dataUri
+       },()=> this.postIdentification())
+     } }
+     />,
+   })
+
+  }
  }
 
  postIdentification = () => {
